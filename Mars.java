b362f849dcbf1f2c82db2c861809ee5e45a5dac3
Koy Dibb <dibbk5@gmail.com>
2023-03-23 20:00:33 -0600

    Final Commit

--- Mars.java
+++ Mars.java
@@ -22,16 +22,32 @@ public class Mars {
             System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
         }
 
+        landing = landingCheck(4);
+
     }
 
     /**
      * Function name: landingCheck
+     * 
      * @param minutesLeft (int)
      * 
-     * Inside the function:
-     * 1. 
+     *                    Inside the function:
+     *                    1. We count down minutes and return how to land.
      */
-    public static void landingCheck(int minutesLeft) {
-        System.out.println(directions)
+    public static boolean landingCheck(int minutesLeft) {
+        int minute = 0;
+        for (int i = minute; i <= minutesLeft; i++) {
+            if (((i % 2) == 0) && ((i % 3) == 0)) {
+                System.out.println("Keep Center");
+            } else if (i % 2 == 0) {
+                System.out.println("Right");
+            } else if (i % 3 == 0) {
+                System.out.println("Left");
+            } else {
+                System.out.println("Calculating");
+            }
+        }
+        System.out.println("Landed");
+        return false;
     }
 }
