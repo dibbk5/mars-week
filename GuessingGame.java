ab6d4e10ec079634148d434b0122b350f80cd880
Koy Dibb <dibbk5@gmail.com>
2023-03-28 18:38:07 -0600

    Scanner Setup

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class GuessingGame {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the Guessing Game!");
+
+        System.out.println("Please enter your name:");
+        String playerName = scan.nextLine();
+
+        scan.close();
     }
 }
